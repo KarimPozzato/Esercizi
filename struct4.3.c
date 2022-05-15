cc883449d9956e75df86531dc8f0c9928f43b245
KarimPozzato <94352240+KarimPozzato@users.noreply.github.com>
2022-05-15 17:05:35 +0200

    Add files via upload

---
+++ struct4.3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define DIM 20
+#define DIM2 5
+
+typedef struct
+{
+    char nome[DIM];
+    char ruolo[DIM];
+    char squadra[DIM];
+} calciatore_T;
+
+int main(int argc, char *argv[])
+{
+    calciatore_T calciatori[DIM2];
+    calciatore_T calciatore;
+    
+    for (int i = 0; i <= DIM2; i++)
+    {
+        printf("Inserire nome calciatore: ");
+        scanf("%s", calciatori[i].nome);
+        printf("Inserisci ruolo: ");
+        scanf("%s", calciatori[i].ruolo);
+        printf("Inserisci squadra: ");
+        scanf("%s", calciatori[i].squadra);
+        printf("\n");
+    }
+
+    printf("Giocatori che iniziano con la lettera 'c' \n");
+    for(int i = 0; i < DIM2; i++)
+    {
+        if(calciatori[i].nome[0] == 'c')
+            printf("\t%s\n", calciatori[i].nome);
+    }
+
+    printf("Giocatori che giocano nel milan");
+    for(int i = 0; i < DIM2; i++)
+    {
+        if(calciatori[i].squadra == "milan")
+            printf("\t%s\n", calciatori[i].nome);
+    }
+}
