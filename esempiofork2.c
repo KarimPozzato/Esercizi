7d357d99af0f7051b9c74627c7c4a9c79229532a
KarimPozzato <94352240+KarimPozzato@users.noreply.github.com>
2023-01-18 13:20:58 +0100

    esempioFork2.c

---
+++ esempiofork2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main()
+{
+    int pid;
+    printf("1) prima della fork\n");
+    pid = fork();
+    printf("2) dopo della fork\n");
+    if (pid == 0)
+    {
+        printf("3) sono il processo figlio con pid %d", getpid());
+        printf("il mio papi ha pid %d\n", getppid());
+        exit(1);
+    }
+    else
+    {
+        printf("3) sono il processo padre con pid %d", getpid());
+        printf("il mio papi ha pid %d\n", getppid());
+        exit(0);
+    }
+}
